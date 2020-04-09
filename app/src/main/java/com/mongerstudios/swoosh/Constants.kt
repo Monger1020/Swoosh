6b500074ed2b41b6b0f747f27bb9c8a63cb06349
Monger <chad1020@gmail.com>
2020-04-09 11:10:20 +0300

    Added Logic to Toggle Button and league next button, and added simple passing of data from activity to next view.

---
+++ app/src/main/java/com/mongerstudios/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.mongerstudios.swoosh
+
+const val EXTRA_LEAGUE = "League"
--- app/src/main/java/com/mongerstudios/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/mongerstudios/swoosh/LeagueActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +16,35 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun LeagueNextClicked(view: View){
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        //transition to Next View
+        if (selectedLeague != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE,selectedLeague)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this,"Select a League first",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //Togle button logic functions
+    fun OnMensClicked(view: View){
+        //btnMens.isChecked = true
+        btnWomens.isChecked = false
+        btnCoed.isChecked = false
+        selectedLeague = "Mens"
+    }
+
+    fun OnWomensClicked(view:View){
+        btnMens.isChecked = false
+        //btnWomens.isChecked = true
+        btnCoed.isChecked = false
+        selectedLeague = "Womens"
+    }
+
+    fun OnCoedClicked(view: View){
+        btnMens.isChecked = false
+        btnWomens.isChecked = false
+        //btnCoed.isChecked = true
+        selectedLeague = "Co-Ed"
     }
 }
